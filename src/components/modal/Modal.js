219e895d6977c28158769abbe0b232582dc47109
Alan Eicker <alaneicker@gmail.com>
2020-03-21 06:24:58 -0500

    Modal style, documentation and logic

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -5,23 +5,38 @@ import PropTypes from 'prop-types';
 const propTypes = {
   classes: PropTypes.string,
   children: PropTypes.node,
-  isOpen: PropTypes.bool
+  isOpen: PropTypes.bool,
+  onClose: PropTypes.func
 };
 
 const defaultProps = {
   classes: '',
   children: null,
-  isOpen: false
+  isOpen: false,
+  onClose() {}
 };
 
-const Modal = ({ classes, children, isOpen, ...others }) => {
+const Modal = ({ classes, children, isOpen, onClose, ...others }) => {
+  const handleClose = e => {
+    const isOverlayClick = e.target.classList.contains('modal');
+    
+    if (isOverlayClick) {
+      onClose();
+    }
+  };
+
   return(
-    <div className={classnames('modal', classes, {
-      'is-open': isOpen
-    })} {...others}>
+    <div 
+      className={classnames('modal', classes, {
+        'is-open': isOpen
+      })} 
+      {...others}
+      onClick={e => handleClose(e)}
+    >
       <div className="modal__dialog">
         <div className="modal__header">
-
+          <div className="modal__title">title</div>
+          <button className="modal__close-button" onClick={() => onClose()}>close</button>
         </div>
         <div className="modal__body">
           {children}
--- src/components/modal/Modal.md
+++ src/components/modal/Modal.md
@@ -4,9 +4,19 @@ import { Modal, Button } from '@alaneicker/react-component-library';
 
 const [isOpen, setIsOpen] = useState(false);
 
+const toggleModal = () => {
+  setIsOpen(!isOpen);
+};
+
 <>
-  <Button variant="primary" size="md">Toggle Modal</Button>
-  <Modal isOpen={isOpen}>
+  <Button 
+    variant="primary" 
+    size="md" 
+    onClick={() => toggleModal()}
+  >
+    Toggle Modal
+  </Button>
+  <Modal isOpen={isOpen} onClose={toggleModal}>
     <div>Modal content...</div>
   </Modal>
 </>
