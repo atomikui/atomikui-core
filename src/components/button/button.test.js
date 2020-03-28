95a1e78e927fc7cd19a377b5577f142548a28bdb
Alan Eicker <alaneicker@gmail.com>
2020-03-28 16:35:11 -0500

    Button test coverage

---
+++ src/components/button/button.test.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-undef */
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import Button from './Button';
+
+configure({ adapter: new Adapter() });
+
+describe('<Button />', () => {
+  let button;
+
+  beforeEach(() => {
+    button = shallow(
+      <Button
+        classes="btn--custom"
+        size="lg"
+        variant="primary"
+        type="submit"
+        block
+        disabled
+      >
+        <span>Submit</span>
+      </Button>,
+    );
+  });
+
+  it('Should render without errors', () => {
+    expect(button.length).toBe(1);
+  });
+
+  it('Should render children', () => {
+    expect(button.find('span').length).toBe(1);
+    expect(button.find('span').text()).toBe('Submit');
+  });
+
+  it('Should render theme based on `varian prop`', () => {
+    expect(button.find('.btn').hasClass('btn--primary')).toBe(true);
+  });
+
+  it('Should accept custom classes', () => {
+    expect(button.find('.btn').hasClass('btn--custom')).toBe(true);
+  });
+
+  it('Should render button as block', () => {
+    expect(button.find('.btn').hasClass('btn--block')).toBe(true);
+  });
+
+  it('Should render button size variant', () => {
+    expect(button.find('.btn').hasClass('btn--lg')).toBe(true);
+  });
+
+  it('Should rbe able to disable button', () => {
+    expect(button.find('.btn').prop('disabled')).toBe(true);
+  });
+});
