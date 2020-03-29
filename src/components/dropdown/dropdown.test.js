df09461c2158bc9077ad428478e6266c105c31b7
Alan Eicker <alaneicker@gmail.com>
2020-03-29 18:56:18 -0500

    Dropdown test coverage

---
+++ src/components/dropdown/dropdown.test.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-undef */
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+import sinon from 'sinon';
+import { mount, configure } from 'enzyme';
+import Dropdown from './Dropdown';
+
+configure({ adapter: new Adapter() });
+
+describe('<Dropdown />', () => {
+  let dropdown;
+
+  beforeEach(() => {
+    dropdown = mount(
+      <Dropdown
+        label="Select a Color"
+        value="Orange"
+        options={[
+          { text: 'Red', value: 'Red' },
+          { text: 'Orange', value: 'Orange' },
+          { text: 'Yellow', value: 'Yellow' },
+          { text: 'Green', value: 'Green' },
+          { text: 'Blue', value: 'Blue' },
+          { text: 'Purple', value: 'Purple' },
+        ]}
+      />,
+    );
+  });
+
+  it('Should render without errors', () => {
+    expect(dropdown.length).toBe(1);
+  });
+
+  it('Should render with 7 options', () => {
+    expect(dropdown.find('select').children().length).toBe(7);
+  });
+
+  it('Should handle an error', () => {
+    dropdown.setProps({ hasError: true, errorText: 'This field is required' });
+
+    expect(dropdown.find('.dropdown').hasClass('has-error')).toBe(true);
+    expect(dropdown.find('.hint--error').length).toBe(1);
+    expect(dropdown.find('.hint--error').text()).toBe('This field is required');
+  });
+
+  it('Should render a hint', () => {
+    dropdown.setProps({ helpText: 'Some helpful text' });
+
+    expect(dropdown.find('.hint').length).toBe(1);
+    expect(dropdown.find('.hint').text()).toBe('Some helpful text');
+  });
+});
