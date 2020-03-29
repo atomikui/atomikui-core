cbd331f03a8ee645e612e66d5a55e599fb2fa2e4
Alan Eicker <alaneicker@gmail.com>
2020-03-29 07:05:38 -0500

    FormField test coverage

--- src/components/form-field/form-field.test.js
+++ src/components/form-field/form-field.test.js
@@ -2,7 +2,7 @@
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
 import sinon from 'sinon';
-import { shallow, configure } from 'enzyme';
+import { mount, configure } from 'enzyme';
 import FormField from './FormField';
 
 configure({ adapter: new Adapter() });
@@ -12,7 +12,9 @@ describe('<FormField />', () => {
   const onChangeSpy = sinon.spy();
 
   beforeEach(() => {
-    formField = shallow(<FormField onChange={onChangeSpy} />);
+    formField = mount(
+      <FormField label="Phone Number" onChange={onChangeSpy} />,
+    );
   });
 
   it('Should render without errors', () => {
@@ -20,12 +22,49 @@ describe('<FormField />', () => {
   });
 
   it('Should render form field based on the type', () => {
+    formField.setProps({ type: 'radio' });
+
     expect(formField.find('input').prop('type')).toBe('text');
+
+    formField.setProps({ type: 'textarea' });
+
+    expect(formField.find('textarea').length).toBe(1);
   });
 
   it('Should trigger onChange callback when form field value changes', () => {
     formField.find('input').simulate('change', { target: { value: 'test' } });
 
     expect(onChangeSpy.called).toBe(true);
+  });
+
+  it('Should render help hint with text', () => {
+    formField.setProps({ helpText: 'Help text' });
+
+    expect(formField.find('.hint').length).toBe(1);
+    expect(formField.find('.hint').text()).toBe('Help text');
+  });
+
+  it('Should render error class', () => {
+    formField.setProps({ hasError: true });
+
+    expect(formField.find('.formfield').hasClass('has-error')).toBe(true);
+  });
+
+  it('Should render error hint with text', () => {
+    formField.setProps({ errorText: 'Error text', hasError: true });
+
+    expect(formField.find('.hint--error').length).toBe(1);
+    expect(formField.find('.hint--error').text()).toBe('Error text');
+  });
+
+  it('Should not render a label', () => {
+    expect(formField.find('.formfield__label').length).toBe(1);
+    expect(formField.find('.formfield__label').text()).toBe('Phone Number');
+  });
+
+  it('Should not render lable if `labelless prop is set to true`', () => {
+    formField.setProps({ labelless: true });
+
+    expect(formField.find('.formfield__label').length).toBe(0);
   });
 });
