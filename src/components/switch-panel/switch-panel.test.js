a11de6e266bd64cba0aee75c320ba97129c3ffe2
Alan Eicker <alaneicker@gmail.com>
2020-03-29 19:10:19 -0500

    SwitchPanel test coverage

---
+++ src/components/switch-panel/switch-panel.test.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-undef */
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+import sinon from 'sinon';
+import { mount, configure } from 'enzyme';
+import SwitchPanel from './SwitchPanel';
+import Switch from '../switch';
+
+configure({ adapter: new Adapter() });
+
+describe('<SwitchPanel />', () => {
+  let switchPanel;
+  const onChangeSpy = sinon.spy();
+
+  beforeEach(() => {
+    switchPanel = mount(
+      <SwitchPanel>
+        <Switch label="Label" onChange={onChangeSpy} />
+        <Switch label="Label" onChange={onChangeSpy} />
+        <Switch label="Label" onChange={onChangeSpy} />
+      </SwitchPanel>,
+    );
+  });
+
+  it('Should render without errors', () => {
+    expect(switchPanel.length).toBe(1);
+  });
+
+  it('Should render children', () => {
+    expect(switchPanel.find('.switch-panel').children().length).toBe(3);
+  });
+});
