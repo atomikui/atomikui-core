b480c5f3a1aa269263b7fccb57dd68ebe531222d
Alan Eicker <alaneicker@gmail.com>
2020-03-30 14:59:00 -0500

    FormOption test coverage

---
+++ src/components/form-option/form-option.test.js
@@ -0,0 +1,73 @@
+/* eslint-disable no-undef */
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+import sinon from 'sinon';
+import { mount, configure } from 'enzyme';
+import FormOption from './FormOption';
+
+configure({ adapter: new Adapter() });
+
+describe('<FormOption />', () => {
+  let formOption;
+  const formOptionSpy = sinon.spy();
+
+  beforeEach(() => {
+    formOption = mount(
+      <FormOption
+        label="I agree to the terms and conditions"
+        onChange={formOptionSpy}
+      />,
+    );
+  });
+
+  it('Should render without errors', () => {
+    expect(formOption.length).toBe(1);
+  });
+
+  it('Should render checkbox by default', () => {
+    formOption.setProps({ type: 'text' });
+
+    expect(formOption.find('input').prop('type')).toBe('checkbox');
+  });
+
+  it('Should render radio button', () => {
+    formOption.setProps({ type: 'radio' });
+
+    expect(formOption.find('input').prop('type')).toBe('radio');
+  });
+
+  it('Should set name attribute based of ID if no name prop is not set', () => {
+    expect(
+      formOption
+        .find('input')
+        .prop('name')
+        .match(/uid-/),
+    ).not.toBeNull();
+  });
+
+  it('Should trigger onChange callback', () => {
+    formOption.find('input').simulate('change', { checked: true });
+
+    expect(formOptionSpy.called).toBe(true);
+  });
+
+  it('Should handle an error', () => {
+    formOption.setProps({
+      hasError: true,
+      errorText: 'This field is required',
+    });
+
+    expect(formOption.find('.form-option').hasClass('has-error')).toBe(true);
+    expect(formOption.find('.hint--error').length).toBe(1);
+    expect(formOption.find('.hint--error').text()).toBe(
+      'This field is required',
+    );
+  });
+
+  it('Should render a hint', () => {
+    formOption.setProps({ helpText: 'Some helpful text' });
+
+    expect(formOption.find('.hint').length).toBe(1);
+    expect(formOption.find('.hint').text()).toBe('Some helpful text');
+  });
+});
