b1efefb9256340037f70b76318fb652625b1d656
Alan Eicker <alaneicker@gmail.com>
2020-03-30 20:29:16 -0500

    Spinner test coverage

--- src/components/spinner/spinner.test.js
+++ src/components/spinner/spinner.test.js
@@ -8,6 +8,14 @@ configure({ adapter: new Adapter() });
 
 describe('<Spinner />', () => {
   let spinner;
+  const sizes = ['sm', 'md', 'lg', 'xlg'];
+  const colors = [
+    ['default', '#333'],
+    ['red', '#D0191F'],
+    ['orange', '#ff8400'],
+    ['green', '#4fa009'],
+    ['blue', '#027abf'],
+  ];
 
   beforeEach(() => {
     spinner = mount(<Spinner />);
@@ -16,4 +24,17 @@ describe('<Spinner />', () => {
   it('Should render without errors', () => {
     expect(spinner.length).toBe(1);
   });
+
+  test.each(sizes)('Should set the size modifier .spinner--%p', (size) => {
+    spinner.setProps({ size });
+    expect(spinner.find('.spinner').hasClass(`spinner--${size}`)).toBe(true);
+  });
+
+  test.each(colors)(
+    'If color prop is %p it should set spinner svg stroke to %p',
+    (color, stroke) => {
+      spinner.setProps({ color });
+      expect(spinner.find('svg > circle').prop('stroke')).toBe(stroke);
+    },
+  );
 });
