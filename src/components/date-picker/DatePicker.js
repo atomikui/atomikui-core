ac25bdf83663ddc394d435eda42fd0f61c4184ba
Alan Eicker <alaneicker@gmail.com>
2020-04-03 10:25:59 -0500

    Datepicker logic and styles

--- src/components/date-picker/DatePicker.js
+++ src/components/date-picker/DatePicker.js
@@ -9,23 +9,71 @@ import 'react-calendar/dist/Calendar.css';
 
 // TODO: Creat a Calendar component utilizing the react-calendar module
 
-const DatePicker = ({ classes, onChange, onDateChange, value, ...props }) => {
+const DatePicker = ({
+  classes,
+  disabled,
+  onChange,
+  onDateChange,
+  value,
+  ...props
+}) => {
+  const [originalValue, setOriginalValue] = useState(value);
   const [theValue, setTheValue] = useState(value);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleDateChange = (details) => {
     const date = moment(details).format('MM/DD/YYYY');
     setTheValue(date);
-    onChange(date);
+  };
+
+  const handleCalendarSubmit = () => {
+    setOriginalValue(theValue);
+    onChange(theValue);
+    setIsOpen(false);
+  };
+
+  const handleCalendarCancel = () => {
+    setTheValue(originalValue);
+    setIsOpen(false);
   };
 
   return (
     <div className={classnames('date-picker', classes, {})}>
-      <FormField onChange={onChange} value={theValue} {...props} />
-      <div className="date-picker__calendar">
-        <Calendar onChange={(details) => handleDateChange(details)} />
+      <div className="date-picker__input">
+        <FormField
+          onChange={onChange}
+          value={theValue}
+          disabled={disabled}
+          {...props}
+        />
+        <Button
+          classes="date-picker__input__btn"
+          variant="hollow"
+          size="md"
+          onClick={() => setIsOpen(!isOpen)}
+          disabled={disabled}
+        >
+          Cancel
+        </Button>
+      </div>
+      <div
+        className={classnames('date-picker__calendar', {
+          'is-open': isOpen,
+        })}
+      >
+        <Calendar
+          onChange={(details) => handleDateChange(details)}
+          value={theValue ? new Date(theValue) : ''}
+        />
         <div className="date-picker__calendar__btns">
-          <Button size="md">Cancel</Button>
-          <Button variant="primary" size="md">
+          <Button size="md" onClick={() => handleCalendarCancel()}>
+            Cancel
+          </Button>
+          <Button
+            variant="primary"
+            size="md"
+            onClick={() => handleCalendarSubmit()}
+          >
             OK
           </Button>
         </div>
@@ -37,16 +85,19 @@ const DatePicker = ({ classes, onChange, onDateChange, value, ...props }) => {
 DatePicker.propTypes = {
   /** Adds custom component CSS classes */
   classes: PropTypes.string,
+  /** Disables a date picker form field and calandar. */
+  disabled: PropTypes.bool,
   /** Triggers callback when date picker input value changes */
   onChange: PropTypes.func,
   /** Triggers callback when calendar date changes */
   onDateChange: PropTypes.func,
-  /** The date picker input value */
+  /** The date picker input value. Must be a Date(). */
   value: PropTypes.string,
 };
 
 DatePicker.defaultProps = {
   classes: '',
+  disabled: false,
   onChange() {},
   onDateChange() {},
   value: '',
--- src/components/date-picker/DatePicker.md
+++ src/components/date-picker/DatePicker.md
@@ -5,7 +5,40 @@ import { DatePicker } from '@alaneicker/react-component-library';
   <DatePicker
     label="Select a Date"
     helpText="Expected Format: MM/DD/YYYY"
-    onChange={(date) => setDate(date)}
+    onChange={(date) => console.log(date)}
+    value="04/08/2020"
+  />
+</>;
+```
+
+### Disabled
+
+```jsx
+import { DatePicker } from '@alaneicker/react-component-library';
+
+<>
+  <DatePicker
+    label="Select a Date"
+    helpText="Expected Format: MM/DD/YYYY"
+    onChange={(date) => console.log(date)}
+    value="04/08/2020"
+    disabled
+  />
+</>;
+```
+
+### Read Only
+
+```jsx
+import { DatePicker } from '@alaneicker/react-component-library';
+
+<>
+  <DatePicker
+    label="Select a Date"
+    helpText="Expected Format: MM/DD/YYYY"
+    onChange={(date) => console.log(date)}
+    value="04/08/2020"
+    readOnly
   />
 </>;
 ```
