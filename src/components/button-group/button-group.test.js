62df44b0e3dfb5922813c5a764ffc61e23c80400
Alan Eicker <alaneicker@gmail.com>
2020-04-02 20:37:54 -0500

    Update button-group.test.js

--- src/components/button-group/button-group.test.js
+++ src/components/button-group/button-group.test.js
@@ -1,17 +1,17 @@
 /* eslint-disable no-undef */
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
-import { shallow, configure } from 'enzyme';
+import { mount, configure } from 'enzyme';
 import ButtonGroup from './ButtonGroup';
 import Button from '../button';
 
 configure({ adapter: new Adapter() });
 
-describe('<Component />', () => {
+describe('<ButtonGroup />', () => {
   let buttonGroup;
 
   beforeEach(() => {
-    buttonGroup = shallow(
+    buttonGroup = mount(
       <ButtonGroup label="Favorite Car Brand">
         <Button>Chevrolet</Button>
         <Button classes="is-selected">Ford</Button>
@@ -22,5 +22,19 @@ describe('<Component />', () => {
 
   it('Should render without errors', () => {
     expect(buttonGroup.length).toBe(1);
+  });
+
+  it('Should render help hint with text', () => {
+    buttonGroup.setProps({ helpText: 'Help text' });
+
+    expect(buttonGroup.find('.hint').length).toBe(1);
+    expect(buttonGroup.find('.hint').text()).toBe('Help text');
+  });
+
+  it('Should render error hint with text', () => {
+    buttonGroup.setProps({ errorText: 'Error text', hasError: true });
+
+    expect(buttonGroup.find('.hint--error').length).toBe(1);
+    expect(buttonGroup.find('.hint--error').text()).toBe('Error text');
   });
 });
