b85dadb38a58af4e27e6c0088ca25a4813d06ba7
Alan Eicker <alaneicker@gmail.com>
2020-04-04 08:00:07 -0500

    DatePicker test coverage

--- src/components/date-picker/date-picker.test.js
+++ src/components/date-picker/date-picker.test.js
@@ -8,19 +8,54 @@ configure({ adapter: new Adapter() });
 
 describe('<DatePicker />', () => {
   let datepicker;
+  const originalValue = '04/08/2020';
 
   beforeEach(() => {
     datepicker = mount(
       <DatePicker
         label="Select a Date"
         helpText="Expected Format: MM/DD/YYYY"
-        onChange={(date) => console.log(date)}
-        value="04/08/2020"
+        onChange={(date) => {}}
+        value={originalValue}
       />,
     );
   });
 
   it('Should render without errors', () => {
     expect(datepicker.length).toBe(1);
+  });
+
+  it('Should open calendar', () => {
+    datepicker.find('.date-picker__input__btn').simulate('click');
+
+    expect(datepicker.find('.date-picker__calendar').hasClass('is-open')).toBe(
+      true,
+    );
+  });
+
+  it('Should close calendar on cancel', () => {
+    datepicker.find('.date-picker__input__btn').simulate('click');
+    datepicker
+      .find('.date-picker__calendar__ui__btns > :first-child')
+      .simulate('click');
+
+    expect(datepicker.find('.date-picker__calendar').hasClass('is-open')).toBe(
+      false,
+    );
+  });
+
+  it('Should reset DatePicker input value on cancel', () => {
+    datepicker
+      .find('.react-calendar__tile')
+      .first()
+      .simulate('click');
+
+    datepicker
+      .find('.date-picker__calendar__ui__btns > :first-child')
+      .simulate('click');
+
+    expect(datepicker.find('.formfield__input').prop('value')).toBe(
+      originalValue,
+    );
   });
 });
