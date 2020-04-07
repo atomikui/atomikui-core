31db2e5feaaefd4004c8e63a898466522ab3542f
Alan Eicker <alaneicker@gmail.com>
2020-04-07 14:47:28 -0500

    AutoComplete

--- src/components/auto-complete/AutoComplete.js
+++ src/components/auto-complete/AutoComplete.js
@@ -1,22 +1,86 @@
-import React from 'react';
+/* eslint-disable indent */
+import React, { useState } from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import Downshift from 'downshift';
 import FormField from '../form-field';
 
-const AutoComplete = ({ classes, ...others }) => {
+const AutoComplete = ({ classes, items, onChange, ...others }) => {
+  const [itemList] = useState(items);
+
   return (
-    <div className={classnames('auto-complete', classes, {})} {...others}></div>
+    <Downshift
+      onChange={(selection) => {
+        return onChange(selection.value);
+      }}
+      itemToString={(item) => {
+        return item ? item.value : '';
+      }}
+    >
+      {({
+        getInputProps,
+        getItemProps,
+        getMenuProps,
+        isOpen,
+        inputValue,
+        selectedItem,
+        getRootProps,
+      }) => {
+        return (
+          <div className={classnames('auto-complete', classes)}>
+            <FormField
+              {...getRootProps({}, { suppressRefError: true })}
+              {...getInputProps()}
+              {...others}
+            />
+            <ul className="auto-complete__list" {...getMenuProps()}>
+              {isOpen
+                ? itemList
+                    .filter((item) => {
+                      return !inputValue || item.value.includes(inputValue);
+                    })
+                    .map((item, index) => {
+                      return (
+                        <li
+                          {...(selectedItem === item && {
+                            className: 'is-selected',
+                          })}
+                          {...getItemProps({
+                            key: item.value,
+                            index,
+                            item,
+                          })}
+                        >
+                          {item.value}
+                        </li>
+                      );
+                    })
+                : null}
+            </ul>
+          </div>
+        );
+      }}
+    </Downshift>
   );
 };
 
 AutoComplete.propTypes = {
   /** Adds custom component CSS classes */
   classes: PropTypes.string,
+  /** Array of items that will appear in autocomplete dropdown menu */
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.string,
+    }),
+  ),
+  /** onChange callback triggered when value changes */
+  onChange: PropTypes.func,
 };
 
 AutoComplete.defaultProps = {
   classes: '',
+  items: [],
+  onChange() {},
 };
 
 export default AutoComplete;
---
+++ src/components/auto-complete/AutoComplete.md
@@ -0,0 +1,27 @@
+```jsx
+import { useState } from 'react';
+import { AutoComplete } from '@alaneicker/react-component-library';
+
+const [selected, setSelected] = useState('');
+
+<>
+  <AutoComplete
+    label="Select Your Favorite Fruit"
+    onChange={(value) => setSelected(value)}
+    items={[
+      { value: 'apple' },
+      { value: 'banana' },
+      { value: 'cantaloupe' },
+      { value: 'grape' },
+      { value: 'mango' },
+      { value: 'nectarine' },
+      { value: 'orange' },
+      { value: 'pear' },
+      { value: 'peach' },
+      { value: 'strawberry' },
+      { value: 'tangerine' },
+    ]}
+  />
+  <p>Your favorite fruit is: {selected}</p>
+</>;
+```
