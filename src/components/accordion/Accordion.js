692abd50f37c75b71bc2946c783c13c21b05b18b
Alan Eicker <alaneicker@gmail.com>
2020-04-08 14:48:22 -0500

    Adds logic to control aria-expanded attributes

--- src/components/accordion/Accordion.js
+++ src/components/accordion/Accordion.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
 const Accordion = ({ classes, items, multipleOpen, ...others }) => {
+  const [expanded, setExpanded] = useState(() => {
+    let state = {};
+
+    items.forEach((item, i) => {
+      state = { ...state, [i]: item.expanded || false };
+    });
+
+    return state;
+  });
+
+  const updateAriaExpanded = (type, index, isChecked) => {
+    if (type === 'radio') {
+      const state = {};
+
+      Object.keys(expanded).forEach((key) => {
+        state[key] = +key === index || false;
+      });
+
+      setExpanded(state);
+    } else {
+      setExpanded({ ...expanded, [index]: isChecked });
+    }
+  };
+
   return (
     <div className={classnames('accordion', classes, {})} {...others}>
       {items.map(({ title, content }, i) => {
@@ -13,9 +37,21 @@ const Accordion = ({ classes, items, multipleOpen, ...others }) => {
           : `accordion_ ${type}_control`;
 
         return (
-          <div key={Math.random()} className="accordion__item">
-            <input id={id} type={type} name={name} />
-            <label className="accordion__item__title" htmlFor={id}>
+          <div className="accordion__item" key={Math.random()}>
+            <input
+              id={id}
+              type={type}
+              name={name}
+              checked={expanded[i]}
+              onChange={(e) => {
+                updateAriaExpanded(type, i, e.target.checked);
+              }}
+            />
+            <label
+              className="accordion__item__title"
+              htmlFor={id}
+              aria-expanded={expanded[i]}
+            >
               {title}
             </label>
             <div className="accordion__item__panel">{content}</div>
