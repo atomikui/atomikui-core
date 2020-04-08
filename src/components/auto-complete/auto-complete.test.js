0b4d016de31e5b20d561ac3b62da3c88feaa37e2
Alan Eicker <alaneicker@gmail.com>
2020-04-07 21:05:47 -0500

    autocomplete test coverage

--- src/components/auto-complete/auto-complete.test.js
+++ src/components/auto-complete/auto-complete.test.js
@@ -1,13 +1,48 @@
 /* eslint-disable no-undef */
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
-import { shallow, configure } from 'enzyme';
+import sinon from 'sinon';
+import { mount, configure } from 'enzyme';
 import AutoComplete from './AutoComplete';
 
 configure({ adapter: new Adapter() });
 
 describe('<AutoComplete />', () => {
-  beforeEach(() => {});
+  let autocomplete;
+  let onChangeSpy;
 
-  it('Should render withput errors', () => {});
+  beforeEach(() => {
+    onChangeSpy = sinon.spy();
+
+    autocomplete = mount(
+      <AutoComplete
+        label="Select Your Favorite Fruit"
+        onChange={onChangeSpy}
+        items={[
+          { value: 'Apple' },
+          { value: 'Banana' },
+          { value: 'Cantaloupe' },
+          { value: 'Grape' },
+          { value: 'Mango' },
+          { value: 'Nectarine' },
+          { value: 'Orange' },
+          { value: 'Pear' },
+          { value: 'Peach' },
+          { value: 'Strawberry' },
+          { value: 'Tangerine' },
+        ]}
+      />,
+    );
+  });
+
+  it('Should render withput errors', () => {
+    expect(autocomplete.length).toBe(1);
+  });
+
+  it('Should trigger onChange callback', () => {
+    autocomplete.find('input').simulate('change');
+    autocomplete.find('.auto-complete__list > :last-child').simulate('click');
+
+    expect(onChangeSpy.withArgs('Tangerine').called).toBe(true);
+  });
 });
