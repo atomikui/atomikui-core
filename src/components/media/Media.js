e0b5728dd851626c341a3a29027f1ce20edcdb46
Alan Eicker <alaneicker@gmail.com>
2020-04-09 06:36:07 -0500

    Media component and styles

--- src/components/media/Media.js
+++ src/components/media/Media.js
@@ -2,17 +2,44 @@ import React from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
-const Media = ({ classes, ...others }) => {
-  return <div className={classnames('media', classes, {})} {...others}></div>;
+const Media = ({ align, body, classes, header, inline, footer, ...others }) => {
+  return (
+    <div
+      className={classnames('media', classes, {
+        'media--inline': inline,
+        [`media--align-${align}`]: align,
+      })}
+      {...others}
+    >
+      <div className="media__hd">{header}</div>
+      <div className="media__bd">{body}</div>
+      <div className="media__ft">{footer}</div>
+    </div>
+  );
 };
 
 Media.propTypes = {
+  /** Specifies media content alignment */
+  align: PropTypes.oneOf(['', 'middle', 'bottom']),
   /** Adds custom component CSS classes */
   classes: PropTypes.string,
+  /** Media body content */
+  body: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  /** Media footer content */
+  footer: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  /** Media header content */
+  head: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  /** Displays media content as inline-flex */
+  inline: PropTypes.bool,
 };
 
 Media.defaultProps = {
+  align: '',
   classes: '',
+  body: '',
+  header: '',
+  footer: '',
+  inline: false,
 };
 
 export default Media;
---
+++ src/components/media/Media.md
@@ -0,0 +1,26 @@
+```jsx
+import { Media, Button } from '@alaneicker/react-component-library';
+
+<Media
+  align="middle"
+  header={
+    <img
+      src="https://i.ya-webdesign.com/images/avatar-png-1.png"
+      width="56"
+      height="56"
+      alt="profile image"
+    />
+  }
+  body={
+    <>
+      <h3>John Smith</h3>
+      <div>Age 43</div>
+    </>
+  }
+  footer={
+    <Button size="md" variant="primary">
+      View Profile
+    </Button>
+  }
+/>;
+```
