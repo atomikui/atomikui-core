27785dba5c3ce113b034a2e47933659f8fddfac7
Alan Eicker <alaneicker@gmail.com>
2020-04-08 20:59:56 -0500

    Accordion test coverage

--- src/components/accordion/accordion.test.js
+++ src/components/accordion/accordion.test.js
@@ -7,7 +7,63 @@ import Accordion from './Accordion';
 configure({ adapter: new Adapter() });
 
 describe('<Accordion />', () => {
-  beforeEach(() => {});
+  let accordion;
 
-  it('Should render withput errors', () => {});
+  beforeEach(() => {
+    accordion = mount(
+      <Accordion
+        panels={[
+          { label: 'Panel Heading 1', content: 'Panel 1 content...' },
+          { label: 'Panel Heading 2', content: 'Panel 2 content...' },
+          { label: 'Panel Heading 3', content: 'Panel 3 content...' },
+        ]}
+      />,
+    );
+  });
+
+  it('Should render withput errors', () => {
+    expect(accordion.length).toBe(1);
+  });
+
+  it('Should expand a panel', () => {
+    accordion
+      .find('input')
+      .first()
+      .simulate('change');
+
+    expect(
+      accordion
+        .find('.accordion__panel__label')
+        .first()
+        .prop('aria-expanded'),
+    ).toBe(true);
+  });
+
+  it('Should expand a multiple panels', () => {
+    accordion.setProps({ multipleOpen: true });
+
+    accordion
+      .find('input')
+      .first()
+      .simulate('change', { target: { checked: true } });
+
+    accordion
+      .find('input')
+      .last()
+      .simulate('change', { target: { checked: true } });
+
+    expect(
+      accordion
+        .find('.accordion__panel__label')
+        .first()
+        .prop('aria-expanded'),
+    ).toBe(true);
+
+    expect(
+      accordion
+        .find('.accordion__panel__label')
+        .last()
+        .prop('aria-expanded'),
+    ).toBe(true);
+  });
 });
