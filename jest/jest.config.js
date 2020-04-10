7e64716f012b42934dcde7a28148f30e08cdb15d
Alan Eicker <alaneicker@gmail.com>
2020-04-09 21:17:46 -0500

    Updates coverage report

--- jest/jest.config.js
+++ jest/jest.config.js
@@ -43,7 +43,7 @@ module.exports = {
     [
       './node_modules/jest-html-reporter',
       {
-        pageTitle: 'Test Coverage Report',
+        pageTitle: 'Jest Coverage Report',
       },
     ],
   ],
