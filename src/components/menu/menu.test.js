60f2addd6ae3109143022eaadf7dc0931c4c2e13
Alan Eicker <alaneicker@gmail.com>
2020-04-20 22:14:49 -0500

    Menu test coverage

---
+++ src/components/menu/menu.test.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-undef */
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import Menu from './Menu';
+import Link from '../link';
+
+configure({ adapter: new Adapter() });
+
+describe('<Menu />', () => {
+  let menu;
+
+  beforeEach(() => {
+    menu = shallow(
+      <Menu>
+        <Link href="/home">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/services">Services</Link>
+        <Link href="/blocg">Blog</Link>
+        <Link href="/contact">Contact</Link>
+      </Menu>,
+    );
+  });
+
+  it('Should render without errors', () => {
+    expect(menu.length).toBe(1);
+  });
+
+  it('Should render children', () => {
+    expect(menu.children().length).toBe(5);
+  });
+});
