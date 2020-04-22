ea2d00b47778b3b5102f4a8ec8620909990513cd
Alan Eicker <alaneicker@gmail.com>
2020-04-22 11:46:48 -0500

    Updates Modal to use Icon component

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -2,6 +2,8 @@ import React, { useRef, useEffect, useState } from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import createFocusTrap from 'focus-trap';
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import Overlay from '../overlay';
 
 const Modal = ({
@@ -81,23 +83,7 @@ const Modal = ({
               return onClose();
             }}
           >
-            <svg
-              version="1.1"
-              xmlns="http://www.w3.org/2000/svg"
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              x="0px"
-              y="0px"
-              viewBox="0 0 60.963 60.842"
-              xmlSpace="preserve"
-            >
-              <path
-                d="M59.595,52.861L37.094,30.359L59.473,7.98c1.825-1.826,1.825-4.786,0-6.611
-	c-1.826-1.825-4.785-1.825-6.611,0L30.483,23.748L8.105,1.369c-1.826-1.825-4.785-1.825-6.611,0c-1.826,1.826-1.826,4.786,0,6.611
-	l22.378,22.379L1.369,52.861c-1.826,1.826-1.826,4.785,0,6.611c0.913,0.913,2.109,1.369,3.306,1.369s2.393-0.456,3.306-1.369
-	l22.502-22.502l22.501,22.502c0.913,0.913,2.109,1.369,3.306,1.369s2.393-0.456,3.306-1.369
-	C61.42,57.647,61.42,54.687,59.595,52.861z"
-              />
-            </svg>
+            <Icon icon={faTimes} size="2x" color="white" />
           </button>
         </div>
         <div className="rcl-modal__body">{children}</div>
