31ac46835e4e4a19cbf08d7aac1105358fce3a0d
Alan Eicker <alaneicker@gmail.com>
2020-04-28 11:32:33 -0500

    Fixes  List test coverage

--- src/components/list/list.test.js
+++ src/components/list/list.test.js
@@ -3,7 +3,7 @@ import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
 import { shallow, configure } from 'enzyme';
 import List from './List';
-import ListItem from '../list-item';
+import ListItem from './ListItem';
 
 configure({ adapter: new Adapter() });
 
