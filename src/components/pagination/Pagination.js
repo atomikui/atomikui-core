688774b17936eeaa1e20d668152f808d45d01e4f
Alan Eicker <alaneicker@gmail.com>
2020-04-30 21:57:34 -0500

    Pagination dark mode styles

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -1,12 +1,20 @@
-import React, { Children } from 'react';
+import React, { Children, useContext } from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import generateId from '../../utilities/generateId';
 import { List, ListItem } from '../list';
+import ThemeContext from '../../themeContext';
 
 const Pagination = ({ children, className, ...others }) => {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <List className={classnames('rcl-pagination', className, {})} {...others}>
+    <List
+      className={classnames('rcl-pagination', className, {
+        [`rcl-pagination--${theme}`]: theme,
+      })}
+      {...others}
+    >
       {Children.map(children, (child) => {
         return (
           <ListItem
