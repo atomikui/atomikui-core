30f3f6ebbe256bef461eb0e65d9f0cce31854722
Alan Eicker <alaneicker@gmail.com>
2020-05-01 21:02:56 -0500

    Tabs test coverage

--- src/components/tabs/tabs.test.js
+++ src/components/tabs/tabs.test.js
@@ -2,12 +2,35 @@
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
 import { shallow, configure } from 'enzyme';
+import sinon from 'sinon';
 import Tabs from './Tabs';
+import Tab from './Tab';
+import TabPanel from './TabPanel';
 
 configure({ adapter: new Adapter() });
 
-describe('<Component />', () => {
-  beforeEach(() => {});
+describe('<Tabs />', () => {
+  let tabs;
+  let onChangeSpy;
 
-  it('Should render without errors', () => {});
+  beforeEach(() => {
+    onChangeSpy = sinon.spy();
+
+    tabs = shallow(
+      <div>
+        <Tabs onChange={onChangeSpy}>
+          <Tab label="Tab One" />
+          <Tab label="Tab Two" />
+          <Tab label="Tab Three" />
+        </Tabs>
+        <TabPanel state={[1, 0]}>Item One</TabPanel>
+        <TabPanel state={[1, 1]}>Item Two</TabPanel>
+        <TabPanel state={[1, 2]}>Item Three</TabPanel>
+      </div>,
+    );
+  });
+
+  it('Should render without errors', () => {
+    expect(tabs.length).toBe(1);
+  });
 });
