2dd062c7b88b70fc3ab0ffff896a1d078e045549
Alan Eicker <alaneicker@gmail.com>
2020-05-04 14:03:52 -0500

    Tabs test coverage

--- src/components/tabs/tabs.test.js
+++ src/components/tabs/tabs.test.js
@@ -1,11 +1,10 @@
 /* eslint-disable no-undef */
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
-import { shallow, configure } from 'enzyme';
+import { mount, configure } from 'enzyme';
 import sinon from 'sinon';
 import Tabs from './Tabs';
 import Tab from './Tab';
-import TabPanel from './TabPanel';
 
 configure({ adapter: new Adapter() });
 
@@ -16,21 +15,24 @@ describe('<Tabs />', () => {
   beforeEach(() => {
     onChangeSpy = sinon.spy();
 
-    tabs = shallow(
+    tabs = mount(
       <div>
         <Tabs onChange={onChangeSpy}>
           <Tab label="Tab One" />
           <Tab label="Tab Two" />
           <Tab label="Tab Three" />
         </Tabs>
-        <TabPanel state={[1, 0]}>Item One</TabPanel>
-        <TabPanel state={[1, 1]}>Item Two</TabPanel>
-        <TabPanel state={[1, 2]}>Item Three</TabPanel>
       </div>,
     );
   });
 
   it('Should render without errors', () => {
     expect(tabs.length).toBe(1);
+  });
+
+  it('Should trigger onChange when a tab is clicked', () => {
+    tabs.find('Tab').first().simulate('click');
+
+    expect(onChangeSpy.called).toBe(true);
   });
 });
