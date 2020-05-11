141e20cb95dff281ff04707ea917325f2980d00c
Alan Eicker <alaneicker@gmail.com>
2020-05-11 07:06:15 -0500

    Statistic test coverage

--- src/components/statistic/statistic.test.js
+++ src/components/statistic/statistic.test.js
@@ -3,6 +3,7 @@ import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
 import { shallow, configure } from 'enzyme';
 import Statistic from './Statistic';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
 
 configure({ adapter: new Adapter() });
 
@@ -15,5 +16,22 @@ describe('<Statistic />', () => {
 
   it('Should render without errors', () => {
     expect(statistic.length).toBe(1);
+  });
+
+  it('Should render with the label on the top', () => {
+    statistic.setProps({ topLabel: true });
+
+    expect(statistic.hasClass('rcl-statistic--top-label')).toBe(true);
+  });
+
+  it('Should render horizontal format', () => {
+    statistic.setProps({ horizontal: true });
+
+    expect(statistic.hasClass('rcl-statistic--horizontal')).toBe(true);
+  });
+
+  it('Should render with an icon', () => {
+    statistic.setProps({ icon: faUsers });
+    expect(statistic.find('.rcl-statistic__icon').length).toBe(1);
   });
 });
