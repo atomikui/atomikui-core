161981984994c703a2d0570c26f8f1cc0aa3716e
Alan Eicker <alaneicker@gmail.com>
2020-05-13 21:29:46 -0500

    Portal test coverage setup

---
+++ src/components/portal/portal.test.js
@@ -0,0 +1,12 @@
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import Portal from './Portal';
+
+configure({ adapter: new Adapter() });
+
+describe('<Portal />', () => {
+  beforeEach(() => {});
+
+  it('Should render without errors', () => {});
+});
