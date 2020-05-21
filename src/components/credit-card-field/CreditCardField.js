0d50ce27a74d05d7e24fc4b9fb4c07f9a21519d4
Alan Eicker <alaneicker@gmail.com>
2020-05-21 09:45:43 -0500

    CreditCardField field generation and customFields

--- src/components/credit-card-field/CreditCardField.js
+++ src/components/credit-card-field/CreditCardField.js
@@ -6,13 +6,39 @@ import FormField from '../form-field';
 import Hint from '../hint';
 import { List, ListItem } from '../list';
 
-const CreditCardField = ({ className, fields, label, onChange, ...others }) => {
+const CreditCardField = ({
+  className,
+  customFields,
+  label,
+  onChange,
+  ...others
+}) => {
   const state = useState({
     number: '',
     expiry: '',
     cvc: '',
     zip: '',
   });
+
+  const fields = [
+    ...[
+      {
+        placeholder: 'Card Number',
+        fieldName: 'cc_number',
+        hasError: false,
+        errorText: 'Card number is required',
+      },
+      {
+        placeholder: 'Card Expiration',
+        fieldName: 'cc_expiry',
+        hasError: false,
+        errorText: 'Card expiration date is required',
+      },
+    ],
+    ...customFields,
+  ];
+
+  console.log(fields);
 
   const hasErrors = fields
     .map(({ hasError }) => {
@@ -36,7 +62,18 @@ const CreditCardField = ({ className, fields, label, onChange, ...others }) => {
       >
         <legend className="atomikui-label">{label}</legend>
         <div className="atomikui-credit-card-field__fields">
-          <div className="atomikui-credit-card-field__icon"></div>
+          <svg
+            id="Layer_1"
+            enableBackground="new 0 0 480 480"
+            viewBox="0 0 480 480"
+            xmlns="http://www.w3.org/2000/svg"
+            height="38"
+          >
+            <path
+              fill="#666"
+              d="m472 135h-464c-4.4 0-8-3.6-8-8v-24c0-22.1 17.9-40 40-40h400c22.1 0 40 17.9 40 40v24c0 4.4-3.6 8-8 8zm0 16h-464c-4.4 0-8 3.6-8 8v36c0 4.4 3.6 8 8 8h464c4.4 0 8-3.6 8-8v-36c0-4.4-3.6-8-8-8zm-464 68h464c4.4 0 8 3.6 8 8v150c0 22.1-17.9 40-40 40h-400c-22.1 0-40-17.9-40-40v-150c0-4.4 3.6-8 8-8zm140 117c0-4.4-3.6-8-8-8h-67c-4.4 0-8 3.6-8 8s3.6 8 8 8h67c4.4 0 8-3.6 8-8zm0-50c0-4.4-3.6-8-8-8h-67c-4.4 0-8 3.6-8 8s3.6 8 8 8h67c4.4 0 8-3.6 8-8z"
+            />
+          </svg>
           {fields.map(({ placeholder, fieldName, hasError }) => {
             return (
               <div
@@ -78,7 +115,7 @@ CreditCardField.propTypes = {
   /** Adds custom component CSS classes */
   className: PropTypes.string,
   /** Form fields */
-  fields: PropTypes.arrayOf(
+  customFields: PropTypes.arrayOf(
     PropTypes.shape({
       placeholder: PropTypes.string,
       fieldName: PropTypes.string,
@@ -94,26 +131,7 @@ CreditCardField.propTypes = {
 
 CreditCardField.defaultProps = {
   className: '',
-  fields: [
-    {
-      placeholder: 'Card Number',
-      fieldName: 'cc_number',
-      hasError: false,
-      errorText: 'Card number is required',
-    },
-    {
-      placeholder: 'Card Expiration',
-      fieldName: 'cc_expiry',
-      hasError: false,
-      errorText: 'Card expiration date is required',
-    },
-    {
-      placeholder: 'CVC',
-      fieldName: 'cc_cvc',
-      hasError: false,
-      errorText: 'Card CVC is required',
-    },
-  ],
+  customFields: [],
   label: '',
   onChange() {},
 };
--- src/components/credit-card-field/CreditCardField.md
+++ src/components/credit-card-field/CreditCardField.md
@@ -1,5 +1,21 @@
 ```jsx
 import { CreditCardField } from '@alaneicker/atomik-ui';
 
-<CreditCardField label="Credit Card" />;
+<CreditCardField
+  label="Credit Card"
+  customFields={[
+    {
+      placeholder: 'CVC',
+      fieldName: 'cc_cvc',
+      hasError: false,
+      errorText: 'Card CVC is required',
+    },
+    {
+      placeholder: 'ZIP',
+      fieldName: 'cc_zip',
+      hasError: false,
+      errorText: 'ZIP code is required',
+    },
+  ]}
+/>;
 ```
