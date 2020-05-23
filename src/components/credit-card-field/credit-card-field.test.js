9476276ad121577eb513d16d779cecfb0550f385
Alan Eicker <alaneicker@gmail.com>
2020-05-23 06:26:41 -0500

    CreditCardField test coverage

--- src/components/credit-card-field/credit-card-field.test.js
+++ src/components/credit-card-field/credit-card-field.test.js
@@ -1,12 +1,34 @@
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
-import { shallow, configure } from 'enzyme';
+import sinon from 'sinon';
+import { configure, mount } from 'enzyme';
 import CreditCardField from './CreditCardField';
 
 configure({ adapter: new Adapter() });
 
 describe('<CreditCardField />', () => {
-  beforeEach(() => {});
+  let creditCardField;
+  let onchangeSpy;
+  let onCardNumberChangeSpy;
 
-  it('Should render without errors', () => {});
+  beforeEach(() => {
+    onchangeSpy = sinon.spy();
+    onCardNumberChangeSpy = sinon.spy();
+
+    creditCardField = mount(
+      <CreditCardField
+        label="Credit Card Information"
+        creditCardNumber="6011111111111117"
+        creditCardExpiry="06/22"
+        creditCardCvc="123"
+        creditCardZip="60068"
+        onChange={onchangeSpy}
+        onCardNumberChange={onCardNumberChangeSpy}
+      />,
+    );
+  });
+
+  it('Should render without errors', () => {
+    expect(creditCardField.length).toBe(1);
+  });
 });
