482175b21d0187087b8be2a7b4fed4ebdb53ace6
Alan Eicker <alaneicker@gmail.com>
2020-09-07 11:31:18 -0500

    AutoComplete: Adds forwardRef

--- src/components/auto-complete/AutoComplete.js
+++ src/components/auto-complete/AutoComplete.js
@@ -1,77 +1,79 @@
-import React, { useState } from 'react';
+import React, { useState, forwardRef } from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import Downshift from 'downshift';
 import FormField from '../form-field';
 
-const AutoComplete = ({ className, items, onChange, value, ...others }) => {
-  const [itemList] = useState(items);
+const AutoComplete = forwardRef(
+  ({ className, items, onChange, value, ...others }, ref) => {
+    const [itemList] = useState(items);
 
-  return (
-    <Downshift
-      onChange={(selection) => {
-        return onChange(selection ? selection.value : '');
-      }}
-      itemToString={(item) => {
-        return item ? item.value : '';
-      }}
-      initialInputValue={value}
-      initialSelectedItem={{ value }}
-    >
-      {({
-        getInputProps,
-        getItemProps,
-        getMenuProps,
-        highlightedIndex,
-        isOpen,
-        inputValue,
-        getRootProps,
-        selectedItem,
-      }) => {
-        return (
-          <div
-            className={classnames('atomikui-auto-complete', className)}
-            {...getRootProps({}, { suppressRefError: true })}
-          >
-            <FormField {...getInputProps()} {...others} />
-            <ul className="atomikui-auto-complete__list" {...getMenuProps()}>
-              {isOpen
-                ? itemList
-                    .filter((item) => {
-                      return (
-                        !inputValue ||
-                        item.value
-                          .toLowerCase()
-                          .includes(inputValue.toLowerCase())
-                      );
-                    })
-                    .map((item, index) => {
-                      return (
-                        <li
-                          className={classnames({
-                            'is-selected': selectedItem
-                              ? selectedItem.value === item.value
-                              : false,
-                            'is-highlighted': highlightedIndex === index,
-                          })}
-                          {...getItemProps({
-                            key: item.value,
-                            index,
-                            item,
-                          })}
-                        >
-                          {item.value}
-                        </li>
-                      );
-                    })
-                : null}
-            </ul>
-          </div>
-        );
-      }}
-    </Downshift>
-  );
-};
+    return (
+      <Downshift
+        onChange={(selection) => {
+          return onChange(selection ? selection.value : '');
+        }}
+        itemToString={(item) => {
+          return item ? item.value : '';
+        }}
+        initialInputValue={value}
+        initialSelectedItem={{ value }}
+      >
+        {({
+          getInputProps,
+          getItemProps,
+          getMenuProps,
+          highlightedIndex,
+          isOpen,
+          inputValue,
+          getRootProps,
+          selectedItem,
+        }) => {
+          return (
+            <div
+              className={classnames('atomikui-auto-complete', className)}
+              {...getRootProps({}, { suppressRefError: true })}
+            >
+              <FormField ref={ref} {...getInputProps()} {...others} />
+              <ul className="atomikui-auto-complete__list" {...getMenuProps()}>
+                {isOpen
+                  ? itemList
+                      .filter((item) => {
+                        return (
+                          !inputValue ||
+                          item.value
+                            .toLowerCase()
+                            .includes(inputValue.toLowerCase())
+                        );
+                      })
+                      .map((item, index) => {
+                        return (
+                          <li
+                            className={classnames({
+                              'is-selected': selectedItem
+                                ? selectedItem.value === item.value
+                                : false,
+                              'is-highlighted': highlightedIndex === index,
+                            })}
+                            {...getItemProps({
+                              key: item.value,
+                              index,
+                              item,
+                            })}
+                          >
+                            {item.value}
+                          </li>
+                        );
+                      })
+                  : null}
+              </ul>
+            </div>
+          );
+        }}
+      </Downshift>
+    );
+  },
+);
 
 AutoComplete.propTypes = {
   /** Adds custom component CSS classes */
