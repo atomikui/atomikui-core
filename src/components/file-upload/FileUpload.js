e7637519df8a694599dc6f27d995a2ee7c0293ad
Alan Eicker <alaneicker@gmail.com>
2020-09-07 11:36:36 -0500

    FileUpload: Adds forwardRef

--- src/components/file-upload/FileUpload.js
+++ src/components/file-upload/FileUpload.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, forwardRef } from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import shortid from 'shortid';
@@ -8,119 +8,127 @@ import List from '../list';
 import ListItem from '../list-item';
 import Hint from '../hint/Hint';
 
-const FileUpload = ({
-  className,
-  dragAndDrop,
-  errorText,
-  hasError,
-  helpText,
-  label,
-  name,
-  onChange,
-  uploadBtnTheme,
-  ...others
-}) => {
-  const [files, setFiles] = useState([]);
+const FileUpload = forwardRef(
+  (
+    {
+      className,
+      dragAndDrop,
+      errorText,
+      hasError,
+      helpText,
+      label,
+      name,
+      onChange,
+      uploadBtnTheme,
+      ...others
+    },
+    ref,
+  ) => {
+    const [files, setFiles] = useState([]);
 
-  const id = shortid.generate();
-  const inputName = name || id;
-  const inputHintId = `${inputName}_hint`;
-  const inputErrorId = `${inputName}_error`;
+    const id = shortid.generate();
+    const inputName = name || id;
+    const inputHintId = `${inputName}_hint`;
+    const inputErrorId = `${inputName}_error`;
 
-  const getFileNames = (fileList) => {
-    const fileArray = [];
+    const getFileNames = (fileList) => {
+      const fileArray = [];
 
-    for (let i = 0; i < fileList.length; i += 1) {
-      fileArray.push(fileList[i].name);
-    }
+      for (let i = 0; i < fileList.length; i += 1) {
+        fileArray.push(fileList[i].name);
+      }
 
-    return fileArray;
-  };
+      return fileArray;
+    };
 
-  const handleChange = (e) => {
-    e.preventDefault();
+    const handleChange = (e) => {
+      e.preventDefault();
 
-    const selectedFiles = e.target.files || e.dataTransfer.files;
+      const selectedFiles = e.target.files || e.dataTransfer.files;
 
-    setFiles(getFileNames(selectedFiles));
-    onChange(selectedFiles);
-  };
+      setFiles(getFileNames(selectedFiles));
+      onChange(selectedFiles);
+    };
 
-  const onDragOver = (e) => {
-    e.stopPropagation();
-    e.preventDefault();
-  };
+    const onDragOver = (e) => {
+      e.stopPropagation();
+      e.preventDefault();
+    };
 
-  return (
-    <div
-      className={classnames('atomikui-file-upload', className, {
-        'has-error': hasError,
-      })}
-    >
-      <input
-        id={id}
-        name="fileUpload"
-        type="file"
-        onChange={handleChange}
-        multiple
-        {...others}
-      />
-      <label
-        onDragOver={onDragOver}
-        onDrop={handleChange}
-        htmlFor={id}
-        className={classnames('atomikui-file-upload__wrapper', {
-          'atomikui-file-upload__wrapper--drag-and-drop': dragAndDrop,
+    return (
+      <div
+        className={classnames('atomikui-file-upload', className, {
+          'has-error': hasError,
         })}
       >
-        {dragAndDrop && (
-          <span className="atomikui-file-upload__drag-drop-label">{label}</span>
-        )}
-        <span
-          className={classnames(
-            'atomikui-file-upload__btn atomikui-btn atomikui-btn--condensed atomikui-btn--nowrap',
-            {
-              [`atomikui-btn--${uploadBtnTheme}`]: uploadBtnTheme,
-              'atomikui-btn--no-radius': !dragAndDrop,
-            },
-          )}
+        <input
+          ref={ref}
+          id={id}
+          name="fileUpload"
+          type="file"
+          onChange={handleChange}
+          multiple
+          {...others}
+        />
+        <label
+          onDragOver={onDragOver}
+          onDrop={handleChange}
+          htmlFor={id}
+          className={classnames('atomikui-file-upload__wrapper', {
+            'atomikui-file-upload__wrapper--drag-and-drop': dragAndDrop,
+          })}
         >
-          {dragAndDrop ? (
-            <Icon
-              icon={faCloudUploadAlt}
-              size="5x"
-              color="#027abf"
-              title={label}
-            />
-          ) : (
-            label
+          {dragAndDrop && (
+            <span className="atomikui-file-upload__drag-drop-label">
+              {label}
+            </span>
           )}
-        </span>
-        {dragAndDrop && files.length > 0 && (
-          <>
-            <div className="margin-top-16" />
-            <List className="atomikui-file-upload__file-list">
-              {files.map((file, index) => {
-                return <ListItem key={shortid.generate()}>{file}</ListItem>;
-              })}
-            </List>
-          </>
+          <span
+            className={classnames(
+              'atomikui-file-upload__btn atomikui-btn atomikui-btn--condensed atomikui-btn--nowrap',
+              {
+                [`atomikui-btn--${uploadBtnTheme}`]: uploadBtnTheme,
+                'atomikui-btn--no-radius': !dragAndDrop,
+              },
+            )}
+          >
+            {dragAndDrop ? (
+              <Icon
+                icon={faCloudUploadAlt}
+                size="5x"
+                color="#027abf"
+                title={label}
+              />
+            ) : (
+              label
+            )}
+          </span>
+          {dragAndDrop && files.length > 0 && (
+            <>
+              <div className="margin-top-16" />
+              <List className="atomikui-file-upload__file-list">
+                {files.map((file, index) => {
+                  return <ListItem key={shortid.generate()}>{file}</ListItem>;
+                })}
+              </List>
+            </>
+          )}
+          {!dragAndDrop && <span>{files.join(', ')}</span>}
+        </label>
+        {(helpText || errorText) && (
+          <div className="atomikui-formfield__hints">
+            {helpText && <Hint id={inputHintId}>{helpText}</Hint>}
+            {hasError && (
+              <Hint id={inputErrorId} type="error">
+                {errorText}
+              </Hint>
+            )}
+          </div>
         )}
-        {!dragAndDrop && <span>{files.join(', ')}</span>}
-      </label>
-      {(helpText || errorText) && (
-        <div className="atomikui-formfield__hints">
-          {helpText && <Hint id={inputHintId}>{helpText}</Hint>}
-          {hasError && (
-            <Hint id={inputErrorId} type="error">
-              {errorText}
-            </Hint>
-          )}
-        </div>
-      )}
-    </div>
-  );
-};
+      </div>
+    );
+  },
+);
 
 FileUpload.propTypes = {
   /** Adds custom component CSS classes */
