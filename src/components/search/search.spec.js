68e0025638472c06caaa60c132f279729fa2059e
Alan Eicker <alaneicker@gmail.com>
2020-10-03 15:48:53 -0500

    [Search] - Adds accessiblity test

---
+++ src/components/search/search.spec.js
@@ -0,0 +1,8 @@
+import generateAccessibilityTests from '../../../scripts/generateAccessibilityTests';
+
+const component = 'Search';
+
+generateAccessibilityTests({
+  path: `Form%20Controls/${component}`,
+  component,
+});
