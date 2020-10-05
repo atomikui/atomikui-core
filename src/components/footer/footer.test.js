81671a7e73e6cf78bdea49f5eaa1ad6aede978c0
Alan Eicker <alaneicker@gmail.com>
2020-10-04 21:17:43 -0500

    [Footer] - Test coverage

--- src/components/footer/footer.test.js
+++ src/components/footer/footer.test.js
@@ -2,11 +2,33 @@ import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
 import { mount, configure } from 'enzyme';
 import Footer from './Footer';
+import { TestScheduler } from 'jest';
 
 configure({ adapter: new Adapter() });
 
 describe('<Footer />', () => {
-  beforeEach(() => {});
+  let footer;
 
-  it('Should render without errors', () => {});
+  beforeEach(() => {
+    footer = mount(
+      <Footer
+        copyrightEntity="Atomik UI"
+        backgroundColor="#263238"
+        navBackgroundColor="#1b282e"
+        linkColor="#fff9c4"
+        textColor="#fff"
+        logo={<img />}
+      >
+        <a href="/about">About</a>
+        <a href="/products">Products</a>
+        <a href="/faqs">FAQs</a>
+        <a href="/blog">Blog</a>
+        <a href="/contact">Contact</a>
+      </Footer>,
+    );
+  });
+
+  it('Should render without errors', () => {
+    expect(footer).toBeTruthy();
+  });
 });
