8b2b8c566af51faf6bc2c69d5d9a78ede6b0a700
Alan Eicker <alaneicker@gmail.com>
2020-10-11 11:24:36 -0500

    [ESLint Atomik UI] - Adds accessibility test coverage table.spec.js

---
+++ src/components/table/table.spec.js
@@ -0,0 +1,8 @@
+import generateAccessibilityTests from '../../../scripts/generateAccessibilityTests';
+
+const component = 'Table';
+
+generateAccessibilityTests({
+  path: `Data%20Display/${component}`,
+  component,
+});
