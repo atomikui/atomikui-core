4a6b515856e0afe75aaba839e334dc08b5db3ac4
Alan Eicker <alaneicker@gmail.com>
2021-02-09 08:50:21 -0600

    refactoring

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -13,7 +13,6 @@ const Gallery = ({ className, showFeaturedImage, items, ...others }) => {
   const [images, setImages] = useState(null);
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [showModal, setShowModal] = useState(false);
-  const [cachedImages, setCachedImages] = useState([]);
 
   const showImageModal = (index) => {
     setSelectedIndex(index);
@@ -22,33 +21,42 @@ const Gallery = ({ className, showFeaturedImage, items, ...others }) => {
 
   useEffect(() => {
     setImages(
-      shuffle(items).map(({ url, caption }, index) => ({
-        id: `image-${index + 1}`,
-        image: <img key={`image-${index + 1}`} src={url} alt={caption} />,
-        url,
-        caption,
-      })),
+      shuffle(items).map(({ url, caption }, index) => {
+        const id = `image-${index + 1}`;
+        return {
+          id,
+          image: <img key={id} src={url} alt={caption} aria-describedby={id} />,
+          button: (
+            <button
+              className={classnames({
+                'is-featured': index === 0 && showFeaturedImage,
+              })}
+              key={id}
+              style={{ background: `no-repeat center/cover url(${url})` }}
+              onClick={() => showImageModal(index)}
+              aria-label={`show image ${caption}`}
+            />
+          ),
+          url,
+          caption,
+        };
+      }),
     );
-  }, [items]);
+  }, [items, showFeaturedImage]);
 
   return images ? (
     <>
       <div className={classnames('atomikui-gallery', className)} {...others}>
-        {images.map(({ url, caption }, index) => (
-          <button
-            className={classnames({
-              'is-featured': index === 0 && showFeaturedImage,
-            })}
-            key={`image-${index + 1}`}
-            style={{ background: `no-repeat center/cover url(${url})` }}
-            onClick={() => showImageModal(index)}
-            aria-label={`show image ${caption}`}
-          />
-        ))}
+        {images.map(({ button }) => button)}
       </div>
       <Overlay className="atomikui-gallery-overlay" isActive={showModal}>
         <div className="atomikui-gallery-overlay__image">
-          <Button theme="black" size="md" onClick={() => setShowModal(false)}>
+          <Button
+            theme="black"
+            size="md"
+            aria-label="close modal"
+            onClick={() => setShowModal(false)}
+          >
             <Icon icon={faTimes} size="lg" color="white" />
           </Button>
           {images[selectedIndex].image}
