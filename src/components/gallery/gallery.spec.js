e66dcf451cc0d767a793bf476df88e5254171e4e
Alan Eicker <alaneicker@gmail.com>
2021-02-09 14:48:41 -0600

    Updates

---
+++ src/components/gallery/gallery.spec.js
@@ -0,0 +1,8 @@
+import generateAccessibilityTests from '../../../scripts/generateAccessibilityTests';
+
+const component = 'Gallery';
+
+generateAccessibilityTests({
+  path: `Content/${component}`,
+  component,
+});
