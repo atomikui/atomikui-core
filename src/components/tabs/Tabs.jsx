838639e69e6d6805056e0b197e6549b05bd57bae
Alan Eicker <alaneicker@gmail.com>
2021-04-01 13:35:28 -0500

    Update Tabs.jsx

--- src/components/tabs/Tabs.jsx
+++ src/components/tabs/Tabs.jsx
@@ -1,4 +1,4 @@
-import React, { cloneElement, Children, useState } from 'react';
+import React, { cloneElement, useState } from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import TabButton from './components/tab-button';
